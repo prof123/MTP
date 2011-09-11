dcfce9ef501c4c1fcb26043e738d088b46e1585e
prateek sharma <prateeks@tripitz.(none)>
2011-09-11 20:59:01 +0530

    clean

---
+++ monitor_experiment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# read procfs every 2 seconds and store it. tab separated instead of otherwise?
+# Put timestamps which are consistent with ftrace timestamps.
+# trace on/off ?
+#record other things like start time, wall-clock time.
+# run perf if required?
+# dmesg output?
